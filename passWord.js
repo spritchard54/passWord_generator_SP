bb6635521577cd6eed1e93ec05327f3ba8e266de
Steven Pritchard <stevenpritchard@Stevens-MacBook-Pro.local>
2023-10-11 20:48:40 -0400

    Cleaned up formatting

--- passWord.js
+++ passWord.js
@@ -10,85 +10,77 @@ var specialChar = "!@#$%^&*";
 var passwordLength = '';
 
 // non-starter code - Whatever is returned by this function is what appears in the card
-    var generatePassword = function(){
-    
-        // Steve's Promopts and Confirms
-        // Prompt
-        var passwordLength = window.prompt("Enter a desired password length between 8 and 128 characters");
-        
-            //Convert passwordLength to an integer
-            var passwordLength = (parseInt(passwordLength));
-                console.log("The visitor entered " + passwordLength);
-                console.log("The variable is a " + typeof passwordLength);
-        
-    //Validate the character is within range
-        if(passwordLength < 8 || passwordLength > 128){
-            alert("You character limit must be between 8 & 128 characters");
+var generatePassword = function(){
+
+// Steve's Promopts and Confirms
+// Prompt
+var passwordLength = window.prompt("Enter a desired password length between 8 and 128 characters");
+
+//Convert passwordLength to an integer
+    var passwordLength = (parseInt(passwordLength));
+        console.log("The visitor entered " + passwordLength);
+        console.log("The variable is a " + typeof passwordLength);
+
+//Validate the character is within range
+    if(passwordLength < 8 || passwordLength > 128){
+        alert("You character limit must be between 8 & 128 characters");
             var passwordLength = window.prompt("Enter a desired password length between 8 and 128 characters");
-                console.log("The password you have entered is not between 8 & 128 Characters");
-        
-    // Confirm UPPERCASE characters    
-            }else{
-            var uCase = confirm("Does your password require UPPER CASE characters? Yes (Ok) or No (Cancel)");
-                console.log("UPPERCASE characters = " + uCase);        
-            }
+            console.log("The password you have entered is not between 8 & 128 Characters");
 
-    //Confirm upper case characters
-    //If this condition is true add the corresponding variable to an array, and when all true conditions are in the array, then run the for loop and randomize????
-            var lCase = window.confirm("Does your password require  characters? Yes (Ok) or No (Cancel)");
-                console.log("Lowercase characters = " + lCase); 
+// Confirm UPPERCASE characters    
+        }else{
+        var uCase = confirm("Does your password require UPPER CASE characters? Yes (Ok) or No (Cancel)");
+            console.log("UPPERCASE characters = " + uCase);        
+        }
 
-    //Confirm number of characters
-            var nums = window.confirm("Does your password require numeric characters? Yes (Ok) or No (Cancel)");
-                console.log("Numeric Characters = " + nums);
-            
-    //Confirm special characters    
-            var sChar = window.confirm("Do your password require special characters? Yes (Ok) or No (Cancel)");
-                console.log("Special Characters = " + sChar);
-            
-    //Begin "if statements" and use a loop to generate password; console log there to test if "if statment" is working
+//Confirm upper case characters
+//If this condition is true add the corresponding variable to an array, and when all true conditions are in the array, then run the for loop and randomize????
+    var lCase = window.confirm("Does your password require  characters? Yes (Ok) or No (Cancel)");
+        console.log("Lowercase characters = " + lCase); 
 
-            if(uCase === true){
-                userselections.push(upperCase);        
-                }
-            if(lCase === true){
-                userselections.push(lowerCase);        
-                }
-            if(nums === true){
-                userselections.push(numbers);        
-            }
-            if(sChar === true){
-                userselections.push(specialChar);        
-                }    
-                
-            // createPassword();
+//Confirm number of characters
+    var nums = window.confirm("Does your password require numeric characters? Yes (Ok) or No (Cancel)");
+        console.log("Numeric Characters = " + nums);
 
-                
-                function createPassword(){
-                //Processing - Combine the elements from the userselection into a single string and process on line 66
-                var password = '';
-                for(var i=0; i < passwordLength.length;i++){
-                var randomCharacters = userselections[Math.floor(Math.random()* userselections.length)]; 
-                password += randomCharacters;
-                }
-                //document.querySelector('#password').textContent = password;
-                return password;
-            }         
+//Confirm special characters    
+    var sChar = window.confirm("Do your password require special characters? Yes (Ok) or No (Cancel)");
+        console.log("Special Characters = " + sChar);
 
+//Begin "if statements" and use a loop to generate password; console log there to test if "if statment" is working
+
+    if(uCase === true){
+    userselections.push(upperCase);        
     }
-    
+    if(lCase === true){
+    userselections.push(lowerCase);        
+    }
+    if(nums === true){
+    userselections.push(numbers);        
+    }
+    if(sChar === true){
+    userselections.push(specialChar);        
+    }
+
+//Processing - Combine the elements from the userselection into a single string and process on line 66
+    function createPassword(){
+    var password = '';
+    for(var i=0; i < passwordLength.length;i++){
+    var randomCharacters = userselections[Math.floor(Math.random()* userselections.length)]; 
+    password += randomCharacters;
+    }
+    //document.querySelector('#password').textContent = password;
+    return password;
+    }         
+}//End of function starting on line 12
+
     var userselections = [];
 
-
-
 //Write password to the #password input
-function writePassword() {
+    function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-
-     passwordText.value = password;
-
-     }
+    passwordText.value = password;  
+    }
 
 // Add event listener to generate button - when the user cicks the button it calls the write password function.
     generateBtn.addEventListener("click", writePassword);
